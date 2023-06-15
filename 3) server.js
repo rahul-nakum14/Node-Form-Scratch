e9154beb60d8bbe07185ac5c924f028e403438d1
Rahul Nakum <rahul.nakum14@gmail.com>
2023-06-15 19:44:40 +0530

    Create Server and implement Status Code

--- 3) server.js
+++ 3) server.js
@@ -4,18 +4,43 @@ const fs = require('fs');
 const server = http.createServer((req,res)=>{
 
     res.setHeader('Content-Type','text/html')
-    res.write('<h1>helo world</h1>');
+    // res.write('<h1>helo world</h1>');
+
     // res.end();
 
-    //Sending File
+    let path = './views'
 
-    fs.readFile('./index.html',(err,filedata)=>{
+    switch(req.url){
+        case '/':
+            path+='/index.html'
+            res.statusCode=200;
+            break;
+
+        case '/about-me':
+            res.statusCode=302;
+            res.setHeader('Location','/about');
+            res.end();
+            break;
+
+        case '/about':
+            path+='/about.html'
+            res.statusCode=200;
+            break;
+            
+        default:
+            path+='/404.html'
+            res.statusCode=404;
+            break;
+    };
+
+    //Sending File
+    fs.readFile(path,(err,filedata)=>{
         if(err){
             console.log('file not found');
         }
         else{
-            // res.write();
-            res.end(filedata) // if there is multiple res. then require res.write else res.end is ok...
+            res.write(filedata);
+            res.end(); // if there is multiple res. then require res.write else res.end is ok...
         }
     })
     // console.log(req.url);
